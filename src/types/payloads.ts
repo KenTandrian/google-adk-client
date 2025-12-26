2751baccdf1cd20ec6d9706719669049994b9562
Kenrick Tandrian <kenricktan11@gmail.com>
2025-12-26 15:33:26 +0700

    fix(ts): change payload message type

--- src/types/payloads.ts
+++ src/types/payloads.ts
@@ -1,31 +1,9 @@
-import {
-  Blob,
-  CodeExecutionResult,
-  ExecutableCode,
-  FileData,
-  FunctionCall,
-  FunctionResponse,
-  VideoMetadata
-} from "./google-genai";
+import type { Content } from "./google-genai";
 
 export interface AdkAgentRunSsePayload {
   appName: string;
   userId: string;
   sessionId: string;
-  newMessage: {
-    parts: Array<{
-      videoMetadata?: VideoMetadata;
-      thought?: boolean;
-      inlineData?: Blob;
-      fileData?: FileData;
-      thoughtSignature?: string;
-      codeExecutionResult?: CodeExecutionResult;
-      executableCode?: ExecutableCode;
-      functionCall?: FunctionCall;
-      functionResponse?: FunctionResponse;
-      text?: string;
-    }>;
-    role: string;
-  };
+  newMessage: Content;
   streaming: boolean;
 }
